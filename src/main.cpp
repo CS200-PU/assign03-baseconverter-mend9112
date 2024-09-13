17e88b03876869bef8be6f615300af569fc02ec0
mend9112 <89863735+mend9112@users.noreply.github.com>
2024-09-13 21:04:11 +0000

    Starting assignment 03

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
+// Author:      Marisa Mendosa
+// Date:        09/13/2024 (Replace xx/xx/xxxx with month/day/year)
 // Class:       CS150-xx (Replace xx with your section number)
 // Assignment:  Hello World
 // Purpose:     Display the message Hello World to the screen
@@ -12,9 +12,75 @@
 
 using namespace std;
 
+//***************************************************************************
+// Function:    hexCharToInt
+//
+// Description: Converts hexadecimal value to decimal
+//
+// Parameters:  hexDigit - digit to be converted
+//
+// Returned:    Equivalent decimal number
+//***************************************************************************
+int hexCharToInt (char hexDigit)
+{
+  return 0;
+}
+
+//***************************************************************************
+// Function:    getBase
+//
+// Description: Returns the base value of the value
+//
+// Parameters:  strNumber - value as string the base is to be determined for
+//
+// Returned:    Base value (D, H, B)
+//***************************************************************************
+char getBase (const string &strNumber)
+{
+  return 0;
+}
+
+//***************************************************************************
+// Function:    getNumber
+//
+// Description: 
+//
+// Parameters:  prompt - user inputed value to be converted
+//
+// Returned:    String representing a decimal, hexadecimal, or binary number
+//***************************************************************************
+string getNumber (const string &prompt)
+{
+  return 0;
+}
+
+//***************************************************************************
+// Function:    printTitle
+//
+// Description: Prints the title
+//
+// Parameters:  myTitle - title to print
+//
+// Returned:    None
+//***************************************************************************
+void printTitle (const string &myTitle)
+{
+  cout << myTitle << endl;
+}
+
+
+//***************************************************************************
+// Function:    main
+//
+// Description: Driver for converting base
+//
+// Parameters:  None
+//
+// Returned:    None
+//***************************************************************************
 int main () {
 
-  cout << "Hello World" << endl;
+  printTitle("Hi");
   
   return EXIT_SUCCESS;
 }
